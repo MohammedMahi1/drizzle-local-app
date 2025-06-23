4b532fe82514eb81737d6cf9a89f9d6eecf4fd48
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-23 21:09:56 +0100

    update TaskItem.tsx and Home.tsx

--- src/components/content/TaskItem.tsx
+++ src/components/content/TaskItem.tsx
@@ -1,12 +1,73 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 type TaskProps = {
-  title:string
+  title: string
 }
-const TaskItem = ({title}:TaskProps) => {
+
+const data = [
+      {
+        id: 1,
+        task: "React native",
+        isChecked: false
+      },
+      {
+        id: 2,
+        task: "Laravel 11+",
+        isChecked: false
+      },
+      {
+        id: 3,
+        task: "Next js",
+        isChecked: false
+      },
+      {
+        id: 3,
+        task: "ORM Drizzle",
+        isChecked: false
+      }
+]
+
+
+const TaskTrigger = ({ title }: TaskProps) => {
+  return (
+    <Pressable className='px-8 py-16'>
+      <Text className='text-white text-7xl font-semibold'>{title}</Text>
+    </Pressable>
+  )
+}
+
+
+type TaskItem = {
+  id: number;
+  task: string;
+  isChecked: boolean;
+};
+
+const Tasks = ({ task, isChecked,id }: TaskItem) => {
   return (
     <View>
-      <Text className='text-white text-3xl pb-4'>{title}</Text>
+      <Text className='text-white'>{id}</Text>
+      <Text className='text-white'>{task}</Text>
+      <Text className='text-white'>
+        {
+          isChecked ? "checked" : "not checked"
+        }
+      </Text>
+    </View>
+  )
+}
+
+const TaskItem = ({ title }: TaskProps) => {
+  return (
+    <View className='border-b-2 border-white'>
+      <TaskTrigger title={title} />
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ justifyContent: "space-between" }}
+        data={data}
+        renderItem={({ item }) => <Tasks task={item.task} key={item.id} id={item.id} isChecked={item.isChecked} />}
+      />
     </View>
   )
 }
--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -8,45 +8,46 @@ import { Button } from '../../components/ui/Button';
 import TaskItem from '../../components/content/TaskItem';
 
 const data = [
-  { id: 1, day: "monday" },
-  { id: 2, day: "tuesday" },
-  { id: 3, day: "wednesday" },
-  { id: 4, day: "thursday" },
-  { id: 5, day: "friday" },
-  { id: 6, day: "saturday" },
-  { id: 7, day: "sunday" },
+    { id: 1, day: "monday" },
+    { id: 2, day: "tuesday" },
+    { id: 3, day: "wednesday" },
+    { id: 4, day: "thursday" },
+    { id: 5, day: "friday" },
+    { id: 6, day: "saturday" },
+    { id: 7, day: "sunday" },
 ];
 
 
 const Home = () => {
-    const db = useSQLiteContext();
-    const drizzleDb = drizzle(db, { schema });
-    const getDb = async () => {
-        const result = await drizzleDb.select().from(schema.users);
-        console.log(result);
+    // const db = useSQLiteContext();
+    // const drizzleDb = drizzle(db, { schema });
+    // const getDb = async () => {
+    //     const result = await drizzleDb.select().from(schema.users);
+    //     console.log(result);
 
-    }
-    const addDb = () => {
-        drizzleDb.insert(schema.users).values({
-            name: `User number ${Math.floor(Math.random() * 1000)}`,
-        }).then(()=>console.log("you add new user"))
+    // }
+    // const addDb = () => {
+    //     drizzleDb.insert(schema.users).values({
+    //         name: `User number ${Math.floor(Math.random() * 1000)}`,
+    //     }).then(()=>console.log("you add new user"))
 
-    }
-    const deleteDb = async() => {
-      await drizzleDb.delete(schema.users).then(()=>console.log("Users deleted"))
-    }
+    // }
+    // const deleteDb = async() => {
+    //   await drizzleDb.delete(schema.users).then(()=>console.log("Users deleted"))
+    // }
     return (
-        <Container>
-            <FlatList
+        <FlatList
+            // style={{flex:1}}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ justifyContent: "space-between" }}
             data={data}
-            renderItem={({item})=><TaskItem title={item.day} key={item.id}/>}
-            />
-            <Button onPress={getDb} >Get Users</Button>
-            <Button onPress={addDb}>Add User</Button>
-            <Button onPress={deleteDb}>Delete User</Button>
-            
-        </Container>
+            renderItem={({ item }) => <TaskItem title={item.day} key={item.id} />}
+        />
     )
 }
 
 export default Home
+
+// <Button onPress={getDb} >Get Users</Button>
+// <Button onPress={addDb}>Add User</Button>
+// <Button onPress={deleteDb}>Delete User</Button>

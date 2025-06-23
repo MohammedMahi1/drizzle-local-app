99382c53e2df2c08a11d92096b5c8cae30aff551
MohammedMahi1 <mohammed.mahi012@gmail.com>
2025-06-23 18:14:31 +0100

    finish setuping the drizzle and check the get and insert query

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -7,15 +7,26 @@ import * as schema from '../../../db/schema';
 import { Button } from '../../components/ui/Button';
 
 const Home = () => {
-      const db = useSQLiteContext();
-  const drizzleDb = drizzle(db, { schema});
-  
-  return (
-    <Container>
-        <Text style={{color:"#fff"}}>Home</Text>
-        <Button>Hello World</Button>
-    </Container>
-  )
+    const db = useSQLiteContext();
+    const drizzleDb = drizzle(db, { schema });
+    const getDb = async () => {
+        const result = await drizzleDb.select().from(schema.users);
+        console.log(result);
+
+    }
+    const addDb = () => {
+        drizzleDb.insert(schema.users).values({
+            name: `User number ${Math.floor(Math.random() * 1000)}`,
+        }).then(()=>console.log("you add new user"))
+
+    }
+    return (
+        <Container>
+            <Text style={{ color: "#fff" }}>Home</Text>
+            <Button onPress={getDb} >Get Users</Button>
+            <Button onPress={addDb}>Add User</Button>
+        </Container>
+    )
 }
 
 export default Home
